f5aea85ae1da0c5d77d98ec2417118551d5f1bee
elliot richardson <ear78@aol.com>
2018-03-12 00:19:36 -0500

    refactored server folder and split up code to better organize server files

--- server/index.js
+++ server/index.js
@@ -1,22 +1,16 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback'
-	}, () => {
-		console.log(accessToken)
-	})
-);
+authRoutes(app);
 
-app.get('/auth/google/', passport.authenticate('google', {
-	scope: ['profile', 'email']
-}))
+//OAUTH/PASSPORT IS IN services/passport.js
+
+//AUTH ROUTES ARE IN ROUTES AUTHROUTES.JS
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
