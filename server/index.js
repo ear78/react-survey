86649cb9d61f2dae700e60b018c5bfa8e1ed034d
elliot richardson <ear78@aol.com>
2018-03-15 23:53:43 -0500

    updated google api and secret, client id

--- server/index.js
+++ server/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
-// require('./services/passport');
+require('./services/passport');
 
 const app = express();
 
-// require('./routes/authRoutes')(app);
+require('./routes/authRoutes')(app);
 
 //OAUTH/PASSPORT IS IN services/passport.js
 
 //AUTH ROUTES ARE IN ROUTES AUTHROUTES.JS
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
