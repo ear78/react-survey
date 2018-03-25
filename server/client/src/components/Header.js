4337a789d5be3de56c40035f0fe37b6c70faa0fb
elliot richardson <ear78@aol.com>
2018-03-24 21:14:02 -0500

    added materialize css and updated header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component{
     render(){
         return (
-            <div>
-                Header
-            </div>
+                <nav>
+                    <div className="nav-wrapper">
+                      <Link to="/" className="brand-logo">Emaily</Link>
+                      <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><Link to="login">Login With Google</Link></li>
+                      </ul>
+                    </div>
+                  </nav>
         )
     }
 }
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import 'materialize-css/dist/css/materialize.min.css';
 
 import App from './components/App';
 import reducers from './reducers';
