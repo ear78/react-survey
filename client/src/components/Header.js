f0a525f6bc391821b1afe375b7ed545450201c0e
elliot richardson <ear78@aol.com>
2018-03-27 00:04:37 -0500

    finished adding payments component.  Need to fix env variable because it's not working

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import Payments from './Payments';
+
 class Header extends React.Component{
 
     renderContent() {
@@ -16,11 +18,10 @@ class Header extends React.Component{
                     </li>
                 )
             default:
-                return (
-                    <li>
-                        <a href="/api/logout">Logout</a>
-                    </li>
-                );
+                return [
+                    <li><Payments /></li>,
+                    <li><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
 
